b062b3fcb97f193ecee7922064ccc947b210f156
An Nguyen <anndb1903@gmail.com>
2025-05-03 23:32:09 +0700

    update

--- App.tsx
+++ App.tsx
@@ -2,10 +2,14 @@ import { View, Text } from "react-native";
 import HomeScreen from "./components/review/home";
 import DetailScreen from "./components/review/detail";
 import AboutScreen from "./components/review/about";
+
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { OPENSANS_REGULAR } from "./utils/const";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -22,12 +26,20 @@ const App = () => {
     return null;
   }
 
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <HomeScreen />
-      <DetailScreen />
-      <AboutScreen />
-    </View>
+    // <View>
+    //   <HomeScreen />
+    //   <DetailScreen />
+    //   <AboutScreen />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
